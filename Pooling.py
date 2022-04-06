7460286934c960eb658d999a4f38a66964d40c23
One1h <76420705+One1h@users.noreply.github.com>
2022-04-06 22:01:30 +0800

    Add files via upload

--- Pooling.py
+++ Pooling.py
@@ -1,31 +1,31 @@
 import numpy as np
 
 
-def pooling(input, size=2, mode='max'):
+def pooling(input, size=3, stride=1, padding=2, mode='max'):
+    input = np.pad(array=input, pad_width=((0, 0), (0, 0), (padding, padding), (padding, padding)), mode='constant')
+
     height = input.shape[2]
     width = input.shape[3]
-    size_h = int(np.ceil(height / size))
-    size_w = int(np.ceil(width / size))
+    size_h = (height - size) // stride + 1
+    size_w = (width - size) // stride + 1
 
     out = np.zeros((input.shape[0], input.shape[1], size_h, size_w))
+    print(out.shape)
 
-    for n in range(input.shape[0]):
-        for c in range(input.shape[1]):
-            for h in range(size_h):
-                for w in range(size_w):
-                    if mode == 'average':
-                        out[n, c, h, w] = np.mean(input[n, c,
-                                                  h * size: min((h + 1) * size, height),
-                                                  w * size: min((w + 1) * size, width)])
-                    if mode == 'max':
-                        out[n, c, h, w] = np.max(input[n, c,
-                                                 h * size: min((h + 1) * size, height),
-                                                 w * size: min((w + 1) * size, width)])
+    for h in range(size_h):
+        for w in range(size_w):
+            if mode == 'average':
+                out[:, :, h, w] = np.mean(input[:, :,
+                                          h * stride: min(h * stride + size, height),
+                                          w * stride: min(w * stride + size, width)], axis=(2, 3))
+            if mode == 'max':
+                out[:, :, h, w] = np.max(input[:, :,
+                                         h * stride: min(h * stride + size, height),
+                                         w * stride: min(w * stride + size, width)], axis=(2, 3))
     return out
 
-
 if __name__ == '__main__':
-    input = np.random.random((4, 2, 5, 5))
+    input = np.random.random((1, 1, 3, 3))
     out = pooling(input)
     print(input)
     print(out)
